9faa9c1bdf081096ea2b38bbd27ae51b6fe1069e
Talha Manzoor <talha.manzoor@cialfo.com.sg>
2023-03-23 21:44:19 +0500

    completed Test

--- App.js
+++ App.js
@@ -1,12 +1,45 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { StyleSheet, Text, View, FlatList, Image, TextInput } from 'react-native';
 
 export default function App() {
+  const [users, setUsers] = useState([])
+  const [search, setSearch] = useState('');
+
+  useEffect(() => {
+    fetchUsers()
+  }, [])
+
+  const fetchUsers = () => {
+    fetch("https://randomuser.me/api/?results=5000")
+      .then((response) => response.json())
+      .then((data) => setUsers(data.results))
+      .catch((error) => console.log('--> Error', error))
+  }
+
+  const filterdUsers = users.filter(user =>
+    user.name.first.toLowerCase().includes(search.toLowerCase())
+    || user.name.last.toLowerCase().includes(search.toLowerCase())
+  )
+
   return (
     <View style={styles.container}>
-      <Text style={styles.titleText}> Hello World!</Text>
-      <StatusBar style="auto" />
+      {users.length > 0 && (
+        <TextInput
+          style={styles.searchBar}
+          placeholder='Search first or last name'
+          value={search}
+          onChangeText={(text) => setSearch(text)}
+        />)}
+      <FlatList data={filterdUsers}
+        keyExtractor={(item, index) => index.toString()}
+        renderItem={({ item }) => (
+          <View style={styles.rowContainer} >
+            <Image source={{ uri: item.picture.thumbnail }}
+              style={styles.image}
+            />
+            <Text>{`${item.name.first} ${item.name.last}`}</Text>
+          </View>
+        )} />
     </View>
   );
 }
@@ -24,4 +57,19 @@ const styles = StyleSheet.create({
     fontSize: 20,
     fontWeight: 'bold',
   },
+  searchBar: {
+    borderWidth: 1,
+    padding: 8,
+    margin: 8
+  },
+  rowContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center'
+  },
+  image: {
+    width: 50,
+    height: 50
+  },
+
 });
